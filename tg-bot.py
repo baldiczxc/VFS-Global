658733f4c2b828e6c3a2839e802dc25ab8cf4dd1
Your Name <you@example.com>
2025-06-26 11:11:39 +0300

    commit

--- tg-bot.py
+++ tg-bot.py
@@ -12,7 +12,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 from vfs_parser.monitoring import monitoring
 
-BOT_TOKEN = '7613202366:AAE_U9HXow0ZWIafukQDoJ5MqJoN22ZJS70'
+BOT_TOKEN = '8115279492:AAEXqvfPuYQXMXQI3_bQIKTTkAagjJk03rE'
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
