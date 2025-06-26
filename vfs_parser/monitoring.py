6e293d01db13e4e0464e12d523e31686b772479b
Your Name <you@example.com>
2025-06-26 10:19:11 +0400

    comit

--- vfs_parser/monitoring.py
+++ vfs_parser/monitoring.py
@@ -5,18 +5,18 @@ import time
 from vfs_parser.config.page.page import VisaOpenPage
 from vfs_parser.pages.check_dates_for_all_visa_types_for_one_city import check_dates_for_all_visa_types_for_one_city
 from vfs_parser.pages.login import login_to_vfs
-
+from pyvirtualdisplay import Display
 
 def monitoring():
     ####
     # UNCOMMENT FOR SERVER
     ####
 
-    # display = Display(size=(1920, 1080))
-    # display.start()
+    display = Display(size=(1920, 1080))
+    display.start()
 
     page = VisaOpenPage.create()
     login_to_vfs(page)
     check_dates_for_all_visa_types_for_one_city(page)
 
-        # display.stop()
+    display.stop()
